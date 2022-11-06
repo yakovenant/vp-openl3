00e670b86d319d1bdbf1dd37b3a9f81361fa43d8
yakovenant <yakovenant@mail.ru>
2022-11-07 02:14:05 +0300

    3rd commit

---
+++ train.py
@@ -0,0 +1,14 @@
+import os
+import openl3
+import librosa
+import numpy as np
+import warnings
+from openl3.models import load_audio_embedding_model
+
+
+if __name__ == '__main__':
+    """
+    ...
+    """
+
+    ...
